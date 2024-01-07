dcaa64ab761c7ce06f8fc6807f9788d46562f8e1
AK <155128473+callmeak143@users.noreply.github.com>
2024-01-07 16:27:42 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,36 @@
+class FirstYear
+{
+    void branch()
+    {
+        System.out.println("ECE");
+    }
+    void subjects()
+    {
+         System.out.println("LAC PHY ENG");
+    }
+    void backlogs()
+    {
+         System.out.println("phy");
+    }
+}
+    class SecondYear
+    {
+        void Subjects()
+        {
+            FirstYear f1=new FirstYear();
+            f1.branch();
+             f1.backlogs();
+             System.out.println("CVT SS AC");
+        }
+    }
+    class Test
+    {
+        public static void main(String args[])
+        {
+            SecondYear s1=new SecondYear();
+            s1.Subjects();
+        }
+    }
+
+
+
---
+++ sample 4.java
@@ -0,0 +1,15 @@
+class Sample
+{
+    int a;
+    float b;
+    boolean c;
+    String d;
+    public static void main(String[]args)
+    {
+        Sample s1=new Sample();
+        System.out.println(s1.a);
+         System.out.println(s1.b);
+          System.out.println(s1.c);
+           System.out.println(s1.d);
+    }
+}
